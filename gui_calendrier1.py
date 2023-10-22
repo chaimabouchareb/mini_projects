548869f8f4d4d82f62824af8c84e073b5daaadb6
Chaima Bouchareb <chaimabouchareb17@gmail.com>
2023-10-22 23:57:49 +0000

    Update gui_calendrier1.py

--- gui_calendrier1.py
+++ gui_calendrier1.py
@@ -1,9 +1,21 @@
-from tkinter import ttk
+from tkinter import *
 import ttkbootstrap
+"""from ttkbootstrap.widgets import *
+from ttkbootstrap.dialogs import Querybox
+from ttkbootstrap.dialogs.dialogs import *"""
+import calendar
+root = Window(title="My Calendar", themename="cyborg")
 
-root = ttkbootstrap.window(themename="cyborg")
-root.title('Calendar')
-
+#function to get the date
 def see_date():
     date=cal.entry.get()
     date_label.config(text=date)
+    
+#this is the dataEntry widget
+cal=ttkbootstrap.DateEntry(root, dateformat='%d%m%Y',bootstyle="info")
+cal.pack(padx=40,pady=40)
+#button to get the selected date
+btn=ttk.Button(root,text="Show Date", bootstyle="light-outline", command=see_date)
+btn.pack(padx=40,pady=45)
+date_label=ttk.Label.pack(padx=40,pady=50)
+root.mainloop()
